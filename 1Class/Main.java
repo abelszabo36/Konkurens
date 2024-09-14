3592854b88faff605c8db0597b0b0b2a0f53e2d2
Raffael <70537471+raffaelsimon@users.noreply.github.com>
2024-09-14 14:56:07 +0200

    abc

--- 1Class/Main.java
+++ 1Class/Main.java
@@ -1,2 +1,35 @@
-//micsi skan
-// szia skanom
+public class Main{
+    public static void main(String[] args) {
+       
+    }
+    
+   
+    class Employee{
+        private String name;
+        private int salary;
+
+        public Employee(String name, int salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public int getSalary()
+        {
+            return this.salary;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void increaseSalary(int value)
+        {
+            this.salary += this.salary*value/100;
+        }
+
+    }
+}
+
+   
