d3b1b6170a50914176607e0a62a8ca60411a6234
abelszabo36 <sz.abel221@gmail.com>
2024-09-14 15:09:55 +0200

    GEEKS BRUH

---
+++ 1Class/Employee/Employee.java
@@ -0,0 +1,28 @@
+package Employee;
+
+public class Employee{
+        private String name;
+        private int salary;
+
+        public Employee(String name, int salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public int getSalary()
+        {
+            return this.salary;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void increaseSalary(int value)
+        {
+            this.salary += this.salary*value/100;
+        }
+
+    }
--- 1Class/Main.java
+++ 1Class/Main.java
@@ -1,35 +1,17 @@
+import Employee.*;
+
 public class Main{
     public static void main(String[] args) {
+
+        Employee emp = new Employee("Raffael", 100);
+        System.out.println(emp.getName() + " " + emp.getSalary());
+        emp.increaseSalary(20);
+        System.out.println(emp.getName() + " " + emp.getSalary());
        
     }
     
    
-    class Employee{
-        private String name;
-        private int salary;
-
-        public Employee(String name, int salary)
-        {
-            this.name = name;
-            this.salary = salary;
-        }
-
-        public int getSalary()
-        {
-            return this.salary;
-        }
-
-        public String getName()
-        {
-            return this.name;
-        }
-
-        public void increaseSalary(int value)
-        {
-            this.salary += this.salary*value/100;
-        }
-
-    }
+    
 }
 
    
